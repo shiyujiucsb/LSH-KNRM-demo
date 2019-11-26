88c0d88a90331a5e5138d3885121ce68ef113c35
Shiyu Ji <shiyu@comet-ln3.sdsc.edu>
2019-11-25 16:54:42 -0800

    [Init] Add LSH speed up online code

--- KNRM.cpp
+++ KNRM.cpp
@@ -32,6 +32,9 @@ using LSHFingerprint = std::bitset<N_LSH_BITS>;
 // Embedding dimension.
 constexpr int DIM = 200;
 
+// Number of soft kernels.
+constexpr int N_KERNELS = 10;
+
 namespace nn4ir {
 
 const unsigned SEED =
@@ -83,6 +86,21 @@ std::vector<VectorXd> InitLshRandomVectors() {
     return std::move(vectors);
 }
 
+RMatrixXd InitLSHMatrix() {
+    RMatrixXd M(N_KERNELS, N_LSH_BITS + 1);
+    for (int i = 0; i <= N_LSH_BITS; i++) {
+        double simi = cos(i * 1.0 / N_LSH_BITS * 3.1415926);
+        for (int j = 0; j < (N_KERNELS - 1); j++) {
+            const double diff = -1.0 + 1.0 / (N_KERNELS - 1)
+                                + j * (2.0 / (N_KERNELS - 1)) - simi;
+            M(j, i) =  exp(-100.0 * (diff) * (diff));
+        }
+        M(N_KERNELS - 1, i) += exp(-1e6 * (1.0 - simi) * (1.0 - simi));
+    }
+
+    return std::move(M);
+}
+
 /*
  * Compute ranking score based on original embeddings.
  * */
@@ -121,6 +139,82 @@ double ComputeRankingScore(
             }
             mm(a, iHalfSize) += exp(-1e6 * (1.0 - simi) * (1.0 - simi));
         }
+    }
+    double weight_sum = 0.0;
+    VectorXd vQWeight = VectorXd::Zero(nQSize); //zi
+    for(a = 0 ; a < nQSize; ++ a){
+        double cqw = 2.0;
+        cqw = exp(vW2(0)); 
+        vQWeight(a) = cqw; 
+        weight_sum += vQWeight(a);
+    }
+
+    assert(weight_sum != 0);
+    vQWeight /= weight_sum;
+
+    std::vector<VectorXd> vHi(vW1Size + 1);
+    std::vector<VectorXd> sigma(vW1Size);
+    vHi[0].resize(vW1[0].rows());
+    for(a = 1 ; a < vW1Size + 1; ++ a){
+        vHi[a].resize(vW1[a-1].cols()); 
+        sigma[a-1].resize(vHi[a].size());
+    }
+    for(a = 0; a < mm.rows(); ++ a){
+        vHi[0].setZero();
+        for(int b = 0 ; b < vHi[0].size(); ++ b){
+            vHi[0](b) = log10(mm(a,b) + 1);
+        }
+        //feed forward
+        for(b = 1 ; b < vW1Size + 1; ++ b){ 
+            vHi[b].setZero();
+            vHi[b] = vHi[b-1] * vW1[b-1] + vW3[b-1];
+            vHi[b] = Tanh(vHi[b]);
+        }
+        score += vHi[vW1Size](0) * vQWeight(a);
+    }
+    return score;
+}
+
+/*
+ * Compute ranking score based on LSH fingerprints.
+ * */
+double ComputeRankingScoreFromLSH(
+                const std::unordered_map<int, LSHFingerprint>& id2lsh_mm,
+                const RMatrixXd& lsh_matrix,
+                const std::vector<int>& query_term_ids,
+                const std::vector<int>& doc_term_ids,
+                const std::vector<RMatrixXd>& vW1,
+                const VectorXd& vW2,
+                const std::vector<VectorXd>& vW3){
+    assert(vW1.size() == vW3.size());
+
+    int64_t a,b,c,d,e;
+    constexpr double eps = 1e-4;
+    double score = 0;
+    const int nQSize = query_term_ids.size();
+    const int nVecDim = vW2.size();
+    const int vW1Size = vW1.size();
+    assert(vW1Size > 0);
+    const int iDocLen = doc_term_ids.size();
+
+    if(iDocLen == 0 || nQSize == 0) {
+      return std::numeric_limits<double>::lowest();
+    }
+    RMatrixXd mm = RMatrixXd::Constant(nQSize,vW1[0].rows(),0);
+    const int iHalfSize = vW1[0].rows()-1;
+    for(const int query_term_id : query_term_ids){
+        const LSHFingerprint& query_term_vec =
+                  id2lsh_mm.at(query_term_id);
+        VectorXd hamming_count = VectorXd::Zero(N_LSH_BITS + 1);
+        for(const int doc_term_id : doc_term_ids){
+            const LSHFingerprint& doc_term_vec =
+                      id2lsh_mm.at(doc_term_id);
+            for (int i = 0; i < iHalfSize; i++) {
+                const LSHFingerprint x = query_term_vec ^ doc_term_vec;
+                hamming_count[x.count()] += 1;
+            }
+        }
+        mm.row(a) += lsh_matrix * hamming_count.transpose();
     }
     double weight_sum = 0.0;
     VectorXd vQWeight = VectorXd::Zero(nQSize); //zi
