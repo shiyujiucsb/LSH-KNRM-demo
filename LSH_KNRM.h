3a9af5d301a051766d8ea6f7cbbbbb3325c86ffa
shiyujiucsb <16054786+shiyujiucsb@users.noreply.github.com>
2019-12-09 08:31:50 -0800

    add copyright header

--- LSH_KNRM.h
+++ LSH_KNRM.h
@@ -1,3 +1,26 @@
+/*****************************************************************************
+Copyright (c) 2019 The Regents of the University of California. All Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without modification
+are permitted, following the standand Apache license included in the directory
+
+
+Author: Shiyu Ji
+        Department of Computer Science
+        Unvieresity of California at Santa Barbara
+
+THIS CODE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+*****************************************************************************/
+
 /*
  * Demo: Use LSH to speed up the evaluation of KNRM ranking model.
  */
